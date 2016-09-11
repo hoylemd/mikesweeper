bf04cdb3c0f7549f175df2f443341b2e6e329698
Michael D. Hoyle <hoylemd@gmail.com>
2016-09-10 22:39:05 -0400

    clean up some tic tac toe stuff

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,110 +1,30 @@
 // from game.js
 //   Game
-// from ai.js
-//   AI
+// from tile.js
+//   Tile
 
 function TicTacToeGame() {
   // game-specific stuff to excise
-  var GRID_COLUMNS = 3;
-  var GRID_ROWS = 3;
+  var GRID_COLUMNS = 16;
+  var GRID_ROWS = 16;
 
   var BACKGROUND_COLOUR = 0x999999;
 
   this.width = GRID_COLUMNS * Tile.TILE_WIDTH + 2;
   this.height = GRID_ROWS * Tile.TILE_HEIGHT + 2;
 
-  this.human = {name: 'human'};
-  this.ai = new AI(this);
-  this.player = this.human;
-  this.players = {
-    'human': this.human,
-    'ai': this.ai
-  };
-
   this.cheats = {
-    let_player_win: false,
-    force_coin: null,
   };
 
   this.grid = null;
-  this.victory_line = null;
 
   Game.call(this);
-
-  this.simple_grid = function TicTacToeGame_simple_grid() {
-    var tile_grid = this.grid;
-    var grid = [['','',''],
-                ['','',''],
-                ['','','']]
-
-    //construct simpler grid
-    for (var i = 0; i < 3; i += 1) {
-      for (var j = 0; j < 3; j += 1) {
-        var tile = tile_grid[i][j];
-        grid[i][j] = tile.owner ? tile.owner.name : '';
-      }
-    }
-
-    return grid;
-  };
-
-  function check_line(first, second, third) {
-    return first && first === second && first === third;
-  }
-
-  function no_valid_moves(grid) {
-    return ((grid[0][0] && grid[1][0] && grid[2][0]) &&
-            (grid[0][1] && grid[1][1] && grid[2][1]) &&
-            (grid[0][2] && grid[1][2] && grid[2][2]));
-  }
-
-  this.game_result = function TicTacToeGame_game_result(
-      winner_name, direction, position) {
-    return {
-      winner: this.players[winner_name] || null,
-      direction: direction || null,
-      position: position || null
-    };
-  }
-
-  this.check_for_winner = function TicTacToeGame_check_for_winner(grid) {
-    grid = grid || this.simple_grid();
-
-     for (var i = 0; i < 3; i += 1) {
-      // check vertical
-      if (check_line(grid[i][0], grid[i][1], grid[i][2])) {
-        return this.game_result(grid[i][0], 'vertical', i);
-      }
-
-      // check horizontal
-      if (check_line(grid[0][i], grid[1][i], grid[2][i])) {
-        return this.game_result(grid[0][i], 'horizontal', i);
-      }
-    }
-
-    // check diagonal
-    if (check_line(grid[0][0], grid [1][1], grid[2][2])) {
-      return this.game_result(grid[0][0], 'diagonal', 0);
-    }
-    if (check_line(grid[2][0], grid [1][1], grid[0][2])) {
-      return this.game_result(grid[2][0], 'diagonal', 1);
-    }
-
-    if (no_valid_moves(grid)) {
-      return this.game_result();
-    }
-    return null;
-  };
-
-  this.switch_player = function TicTacToeGame_switch_player() {
-    this.player = this.player === this.human ? this.ai : this.human;
-  };
 
   this.reset = function TicTacToe_reset() {
     this.stage.removeChild(this.lines);
 
-    for (var i = 0; i < 3; i += 1) {
-      for (var j = 0; j < 3; j += 1) {
+    for (var i = 0; i < GRID_COLUMNS; i += 1) {
+      for (var j = 0; j < GRID_ROWS; j += 1) {
         var tile = this.grid[i][j];
         tile.visible = false;
         this.stage.removeChild(tile);
--- assets/js/states.js
+++ assets/js/states.js
@@ -1,31 +1,9 @@
 // from pixi.js:
 //   PIXI
-// from utils.js:
-//   print_grid(grid)
+// from game_state.js
+//   GameState
 // from tile.js:
 //   Tile
-
-function GameState(game) {
-  this.game = game;
-}
-GameState.prototype = {
-  name: 'Unnamed State',
-
-  game: null,
-
-  update: function base_state_update(timedelta) {
-  },
-
-  event_handlers: [],
-  handle_event: function base_state_handle_event(event, object, parameters) {
-    var handler = this.event_handlers[event]
-    if (handler) {
-      handler.call(this, object, parameters);
-      return true;
-    }
-    return false;
-  }
-};
 
 // Global list of states
 var all_states = {};
@@ -34,6 +12,8 @@ function LoadingAssetsState(game) {
   GameState.call(this, game);
 
   this.name = 'loading_assets';
+
+  this.event_handlers = {};
 
   this.loading_started = false;
   this.loading_done = false;
@@ -49,7 +29,7 @@ function LoadingAssetsState(game) {
 
       // Define Textures and Atlases to load here
       var textures = [];
-      var texture_atlases = ['assets/sprites/marks.json'];
+      var texture_atlases = [];
       // Done defining Textures and Atlases
 
       PIXI.loader.add(textures)
@@ -72,54 +52,9 @@ function InitializingState(game) {
 
   this.name = 'initializing';
 
+  this.event_handlers = {};
+
   this.update = function InitializingState_update(timedelta) {
-
-    var lines = new PIXI.Graphics();
-    lines.lineStyle(2, 0x000000, 1);
-
-    lines.moveTo(Tile.TILE_WIDTH + 1, 0);
-    lines.lineTo(Tile.TILE_WIDTH + 1, game.height);
-
-    lines.moveTo(2 * (Tile.TILE_WIDTH + 1), 0);
-    lines.lineTo(2 * (Tile.TILE_WIDTH + 1), game.height);
-
-    lines.moveTo(0, Tile.TILE_WIDTH + 1);
-    lines.lineTo(game.width, Tile.TILE_WIDTH + 1);
-
-    lines.moveTo(0, 2 * (Tile.TILE_WIDTH + 1));
-    lines.lineTo(game.width, 2 * (Tile.TILE_WIDTH + 1));
-
-    game.lines = lines;
-    game.stage.addChild(lines);
-
-    game.tiles = [];
-    game.grid = [];
-
-    for (var i = 0; i < 3; i += 1) {
-      var new_row = [];
-      for (var j = 0; j < 3; j += 1) {
-        var new_tile = new Tile(i, j);
-        new_row[j] = new_tile;
-        game.tiles.push(new_tile);
-        game.game_objects.push(new_tile);
-        game.stage.addChild(new_tile);
-      }
-      game.grid[i] = new_row;
-    }
-
-    game.log("Flipping a coin to see who goes fist. " +
-             "You go first if it's heads!");
-    var coin_flip = Math.random() > 0.5 ? 'heads' : 'tails';
-    coin_flip = game.cheats.force_coin || coin_flip;
-
-    if (coin_flip === 'tails') {
-      game.player = game.ai;
-      game.log('Tails! I go first!');
-    } else {
-      game.player = game.human;
-      game.log('Heads. You go first.');
-    }
-
     this.game.transition('main');
   };
 }
@@ -130,113 +65,15 @@ function MainState(game) {
   GameState.call(this, game);
 
   this.name = 'main';
-  this.turn_taken = false;
 
-  this.claim_tile = function MainState_claim_tile(tile, owner) {
-    if (!tile.set_owner(owner)) {
-      this.game.log('(' + tile.column + ',' + tile.row + ') has already been ' +
-               'claimed by ' + tile.owner.name);
-      return;
-    }
+  this.event_handlers = {};
 
-    if (owner === this.game.human) {
-      this.game.log('You marked (' + tile.column + ',' + tile.row + ').');
-    } else {
-      this.game.log('I choose (' + tile.column + ', ' + tile.row + ').');
-    }
-    this.turn_taken = true;
-  }
-
-  function tile_click(object, arguments) {
-    if (this.game.player === this.game.human) {
-      this.claim_tile(object, this.game.human);
-    }
-  }
-
-  this.event_handlers = {
-    'click': tile_click
-  }
-
-  this.ai_timer = 0;
   this.update = function MainState_update(timedelta) {
-    if (this.game.player === this.game.ai) {
-      this.ai_timer += timedelta;
-
-      if (this.ai_timer >= 500) {
-        this.ai_timer = 0;
-        var choice = this.game.ai.choose_tile();
-        this.claim_tile(choice, this.game.ai);
-      }
-    }
-
-    if (this.turn_taken) {
-      this.turn_taken = false;
-      var result = game.check_for_winner();
-
-      if (result) {
-        this.game.transition('game_over', result);
-      } else {
-        this.game.switch_player();
-      }
-    }
+    console.log('main');
   }
 };
 MainState.prototype = Object.create(GameState.prototype);
 all_states['main'] = MainState;
-
-function GameOverState(game, arguments) {
-  if (arguments && arguments.winner) {
-    if (arguments.winner === game.human) {
-      game.log("Game over. You... won? but... but I'm undefeatable..." +
-               " YOU CHEATED!!!!!!!");
-    } else {
-      game.log("Game over! I win! See? " + arguments.direction + "ly!");
-    }
-
-    // create the line sprite
-    var line_texture = TextureCache['line.png'];
-    var line_sprite = new PIXI.Sprite(line_texture);
-    line_sprite.anchor.x = 0.5;
-    line_sprite.anchor.y = 0.5;
-
-    var PI = 3.14159;
-
-    // position it
-    if (arguments.direction === 'horizontal') {
-      line_sprite.x = 1.5 * Tile.TILE_WIDTH;
-      line_sprite.y = Tile.TILE_HEIGHT * (0.5 + arguments.position);
-    }
-    if (arguments.direction === 'vertical') {
-      line_sprite.rotation = 0.5 * PI;
-      line_sprite.x = Tile.TILE_WIDTH * (0.5 + arguments.position);
-      line_sprite.y = 1.5 * Tile.TILE_HEIGHT;
-    }
-    if (arguments.direction === 'diagonal') {
-      line_sprite.rotation = (0.25 + arguments.position / 2) * PI;
-      line_sprite.x = 1.5 * Tile.TILE_WIDTH;
-      line_sprite.y = 1.5 * Tile.TILE_HEIGHT;
-    }
-    game.stage.addChild(line_sprite);
-    game.victory_line = line_sprite;
-
-  } else {
-    game.log("Game over! it's a draw.");
-  }
-  game.log("Refresh, or click a tile to play again.");
-
-  function tile_click(object, arguments) {
-    game.transition('initializing');
-
-    game.reset();
-  }
-
-  this.event_handlers = {
-    'click': tile_click
-  }
-
-}
-GameOverState.prototype = Object.create(GameState.prototype);
-all_states['game_over'] = GameOverState;
 
 function get_all_states() {
   all_states.__initial__ = 'loading_assets';
