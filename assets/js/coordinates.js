2632b421f2f849cf0e378edf81f4d01a252dbc23
Michael D. Hoyle <hoylemd@gmail.com>
2016-09-20 22:04:53 -0400

    make coordinates requireable

--- assets/js/coordinates.js
+++ assets/js/coordinates.js
@@ -4,8 +4,4 @@ function Coordinates(x, y) {
 }
 Coordinates.prototype = {x: 0, y: 0};
 
-function IndexCoordinates(index) {
-  this.x = index % 3;
-  this.y = Math.floor(index / 3);
-}
-IndexCoordinates.prototype = Object.create(Coordinates.prototype);
+module.exports = Coordinates;
